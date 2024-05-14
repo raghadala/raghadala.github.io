bfa4bde85f004dc8ea160a7e6a02c636879c2ccf
Raghad <selling@RaghadsMac.local>
2024-05-14 03:04:20 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+/*
+const form = document.querySelector('form');
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ficschedulerservice@gmail.com",
+        Password : "B359A187DA6F7B69EBF769D743DE1E6186EC",
+        To : 'ficschedulerservice@gmail.com',
+        From : "ficschedulerservice@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
+*/
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    //reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+}); 
+
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ScrollReveal().reveal('.projects-box', {origin:'bottom'});
+ScrollReveal().reveal('.home-content h1', {origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content, .contact-content', {origin:'right'});
+
+
